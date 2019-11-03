fd72319177c633366111639a308f21b6e35830e1
Gustavo Franco <gustavofrancojp@gmail.com>
2019-11-03 15:28:00 -0400

    slide tela de aferição

--- src/app/pages/afericao/afericao.page.ts
+++ src/app/pages/afericao/afericao.page.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-afericao',
   templateUrl: './afericao.page.html',
-  styleUrls: ['./afericao.page.scss'],
+  styleUrls: ['./afericao.page.scss']
 })
 export class AfericaoPage implements OnInit {
+  afericaoForm: FormGroup;
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    allowTouchMove: false
+  };
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
+
+  @ViewChild('mySlider', { static: true }) slides: IonSlides;
 
   ngOnInit() {
+    this.createForm();
   }
 
+  private createForm(): void {
+    this.afericaoForm = this.formBuilder.group({
+      cnpj: ['', [Validators.required]],
+      nome: ['', [Validators.required]]
+    });
+  }
+
+  nextSlide() {
+    this.slides.slideNext();
+  }
+
+  onSubmit() {
+    console.log(this.afericaoForm.value);
+  }
 }
