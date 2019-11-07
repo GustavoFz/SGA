8bccd83a6223245d98dd156535c4bc95f6ddab49
Gustavo Franco <gustavofrancojp@gmail.com>
2019-11-06 21:18:50 -0400

    Formularios da aferição

--- src/app/pages/afericao/afericao.page.ts
+++ src/app/pages/afericao/afericao.page.ts
@@ -12,6 +12,7 @@ import { ModalBalancasComponent } from 'src/app/components/modal-balancas/modal-
 export class AfericaoPage implements OnInit {
   afericaoForm: FormGroup;
   cliente: any = null;
+  balanca: any = null;
   slideOpts = {
     initialSlide: 0,
     speed: 400
@@ -27,13 +28,22 @@ export class AfericaoPage implements OnInit {
 
   private createForm(): void {
     this.afericaoForm = this.formBuilder.group({
-      cnpj: ['', [Validators.required]],
-      nome: ['', [Validators.required]]
+      pesoCentro: ['', [Validators.required]],
+      pesoCanto1: ['', [Validators.required]],
+      pesoCanto2: ['', [Validators.required]],
+      pesoCanto3: ['', [Validators.required]],
+      pesoCanto4: ['', [Validators.required]],
+      cargaMaxima: ['', [Validators.required]],
+      temperatura: ['', [Validators.required]],
+      umidade: ['', [Validators.required]]
     });
   }
 
   nextSlide() {
     this.slides.slideNext();
+  }
+  prevSlide() {
+    this.slides.slidePrev();
   }
 
   onSubmit() {
@@ -62,6 +72,6 @@ export class AfericaoPage implements OnInit {
     modal.present();
 
     const { data } = await modal.onDidDismiss();
-    this.cliente = data;
+    this.balanca = data;
   }
 }
