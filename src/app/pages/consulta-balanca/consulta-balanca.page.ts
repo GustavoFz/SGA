0ec08c0faab9f9a86a6602f393d95940fdbe764f
Gustavo Franco <gustavofrancojp@gmail.com>
2019-11-07 21:55:55 -0400

    Consulta balancas

--- src/app/pages/consulta-balanca/consulta-balanca.page.ts
+++ src/app/pages/consulta-balanca/consulta-balanca.page.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Balanca } from 'src/app/interfaces/balanca';
+import { BalancaService } from 'src/app/services/balanca.service';
 
 @Component({
   selector: 'app-consulta-balanca',
   templateUrl: './consulta-balanca.page.html',
-  styleUrls: ['./consulta-balanca.page.scss'],
+  styleUrls: ['./consulta-balanca.page.scss']
 })
-export class ConsultaBalancaPage implements OnInit {
+export class ConsultaBalancaPage implements OnInit, OnDestroy {
+  private balancasSubscription: Subscription;
+  public balancas = new Array<Balanca>();
 
-  constructor() { }
+  searchTerm: any = '';
+  balancasFiltered: any;
 
+  constructor(private balancaService: BalancaService) {}
   ngOnInit() {
+    this.balancasSubscription = this.balancaService.getBalancas().subscribe(data => {
+      this.balancas = data;
+      this.balancasFiltered = data;
+    });
   }
 
+  setFilteredItems() {
+    if (this.searchTerm) {
+      this.balancasFiltered = this.balancas.filter(item => {
+        return item.id.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+      });
+    }
+    // else {
+    //   this.clientesFiltered = this.clientes;
+    // }
+  }
+  ngOnDestroy() {
+    this.balancasSubscription.unsubscribe();
+  }
 }
