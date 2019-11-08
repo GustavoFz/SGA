f7765c7fe42fab53e2cfba00f129cf5fd8c1d626
Gustavo Franco <gustavofrancojp@gmail.com>
2019-11-07 23:23:54 -0400

    .

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ModalClientesComponent } from './modal-clientes/modal-clientes.component';
 import { IonicModule } from '@ionic/angular';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { Routes } from '@angular/router';
 import { ModalBalancasComponent } from './modal-balancas/modal-balancas.component';
 import { ModalFerramentasComponent } from './modal-ferramentas/modal-ferramentas.component';
 
 @NgModule({
   imports: [IonicModule, CommonModule, FormsModule],
-  declarations: [ModalClientesComponent, ModalBalancasComponent,ModalFerramentasComponent],
+  declarations: [ModalClientesComponent, ModalBalancasComponent, ModalFerramentasComponent],
   exports: [ModalClientesComponent, ModalBalancasComponent, ModalFerramentasComponent]
 })
 export class ComponentsModule {}
