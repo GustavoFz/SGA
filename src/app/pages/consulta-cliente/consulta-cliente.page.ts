ea14137dbb231724cf2b22cfa09dbd280d7d0ba1
Gustavo Franco <gustavofrancojp@gmail.com>
2019-11-07 21:58:03 -0400

    correção pesquisa cliente

--- src/app/pages/consulta-cliente/consulta-cliente.page.ts
+++ src/app/pages/consulta-cliente/consulta-cliente.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { Subscription } from 'rxjs';
 import { Cliente } from 'src/app/interfaces/cliente';
@@ -8,32 +8,30 @@ import { Cliente } from 'src/app/interfaces/cliente';
   templateUrl: './consulta-cliente.page.html',
   styleUrls: ['./consulta-cliente.page.scss']
 })
-export class ConsultaClientePage implements OnInit {
+export class ConsultaClientePage implements OnInit, OnDestroy {
   private clientesSubscription: Subscription;
   public clientes = new Array<Cliente>();
 
   searchTerm: any = '';
-  jsonData: any;
+  clientesFiltered: any;
 
-  constructor(private clienteService: ClienteService) {
+  constructor(private clienteService: ClienteService) {}
+  ngOnInit() {
     this.clientesSubscription = this.clienteService.getClientes().subscribe(data => {
       this.clientes = data;
+      this.clientesFiltered = data;
     });
-  }
-  ionViewDidLoad() {
-    this.setFilteredItems();
   }
 
-  filterItems(searchTerm) {
-    return this.clientes.filter(item => {
-      return item.nome.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
-    });
-  }
-  ngOnInit() {
-    this.setFilteredItems();
-  }
   setFilteredItems() {
-    this.jsonData = this.filterItems(this.searchTerm);
+    if (this.searchTerm) {
+      this.clientesFiltered = this.clientes.filter(item => {
+        return item.nome.toLowerCase().indexOf(this.searchTerm.toLowerCase()) > -1;
+      });
+    }
+    // else {
+    //   this.clientesFiltered = this.clientes;
+    // }
   }
   ngOnDestroy() {
     this.clientesSubscription.unsubscribe();
